3e9c336d2575416754d8f47d17040c4cb6f459a5
kaqons <123312657+kaqons@users.noreply.github.com>
2024-04-24 22:24:11 -0700

    Update authentication.api.ts

--- apps/web/src/domain/authentication/authentication.api.ts
+++ apps/web/src/domain/authentication/authentication.api.ts
@@ -1,20 +1,24 @@
-import { HttpService } from '../../core/http'
-import { User } from '../user'
+import { HttpService } from '../../core/http';
+import { User } from '../user';
 
 export class AuthenticationApi {
-  static login(values: {
-    email: string
-    password: string
-  }): Promise<{ token?: string }> {
-    return HttpService.api.post(`/v1/authentication/login`, values)
+  static login(values: { email: string; password: string }): Promise<{ token?: string }> {
+    return HttpService.api.post(`/v1/authentication/login`, values);
   }
 
   static register(values: Partial<User>): Promise<{ token?: string }> {
-    return HttpService.api.post(`/v1/authentication/register`, values)
+    return HttpService.api.post(`/v1/authentication/register`, values);
+  }
+
+  static registerGuest(): Promise<{ token?: string }> {
+    // Here you can define the logic to register a guest user
+    // For example, you can generate a temporary user account or assign a role to the guest
+    // This implementation depends on your specific requirements
+    return Promise.resolve({ token: 'guest_token' });
   }
 
   static refresh(): Promise<{ token?: string }> {
-    return HttpService.api.post(`/v1/authentication/refresh`)
+    return HttpService.api.post(`/v1/authentication/refresh`);
   }
 
   static logout(document: Document): Promise<void> {
@@ -22,29 +26,21 @@ export class AuthenticationApi {
       .post(`/v1/authentication/logout`)
       .catch(() => {})
       .then(() => {
-        document.cookie =
-          'APP_ACCESS_TOKEN= ; expires = Thu, 01 Jan 1970 00:00:00 GMT'
-      })
+        document.cookie = 'APP_ACCESS_TOKEN= ; expires = Thu, 01 Jan 1970 00:00:00 GMT';
+      });
   }
 
   static sendResetPassword(email: string): Promise<void> {
-    return HttpService.api.post(`/v1/authentication/reset-password-email`, {
-      email,
-    })
+    return HttpService.api.post(`/v1/authentication/reset-password-email`, { email });
   }
 
   static resetPassword(token: string, password: string): Promise<void> {
-    return HttpService.api.patch(`/v1/authentication/reset-password`, {
-      token,
-      password,
-    })
+    return HttpService.api.patch(`/v1/authentication/reset-password`, { token, password });
   }
 
-  static googleCallback(values: {
-    credential?: string
-  }): Promise<{ token?: string }> {
+  static googleCallback(values: { credential?: string }): Promise<{ token?: string }> {
     return HttpService.api.post(`/v1/authentication/google/callback`, {
       token: values.credential,
-    })
+    });
   }
 }
