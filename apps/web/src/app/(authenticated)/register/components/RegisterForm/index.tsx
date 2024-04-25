4765e5712c2e17720ed41e4f77ea1cdba7aedc98
kaqons <123312657+kaqons@users.noreply.github.com>
2024-04-24 22:20:06 -0700

    Update index.tsx

--- apps/web/src/app/(authenticated)/register/components/RegisterForm/index.tsx
+++ apps/web/src/app/(authenticated)/register/components/RegisterForm/index.tsx
@@ -1,64 +1,20 @@
-'use client'
+import React from 'react';
+import { Button, Form } from 'antd';
 
-import { MrbPasswordStrength } from '@web/designSystem'
-import { Button, Form, Input } from 'antd'
-
-type Props = {
-  isLoading: boolean
-  onSubmit: (values: any) => void
-}
-
-export const RegisterForm = ({ isLoading, onSubmit }: Props) => {
-  const [form] = Form.useForm()
-
-  const handleSubmit = (values: any) => {
-    onSubmit(values)
-  }
-
-  const password = Form.useWatch('password', form)
+const RegisterForm = ({ isLoading, onSubmit }) => {
+  const handleSubmit = () => {
+    onSubmit(); // Call the onSubmit function without any arguments
+  };
 
   return (
-    <Form
-      form={form}
-      onFinish={handleSubmit}
-      layout="vertical"
-      autoComplete="off"
-      requiredMark={false}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Email is required' }]}
-      >
-        <Input type="email" placeholder="Your email" autoComplete="email" />
-      </Form.Item>
-      <Form.Item
-        name="name"
-        rules={[{ required: true, message: 'Name is required' }]}
-        label="Name"
-      >
-        <Input placeholder="Your name" />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Password is required' }]}
-      >
-        <Input.Password
-          type="password"
-          placeholder="Your password"
-          autoComplete="current-password"
-        />
-      </Form.Item>
-
-      <MrbPasswordStrength value={password} className="mb-3" />
-
+    <Form onFinish={handleSubmit}>
       <Form.Item>
         <Button type="primary" htmlType="submit" loading={isLoading} block>
-          Register
+          Register as Guest
         </Button>
       </Form.Item>
     </Form>
-  )
-}
+  );
+};
+
+export default RegisterForm;
