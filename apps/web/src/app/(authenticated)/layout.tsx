e9389c5db726cc4fff3877044e8917450636c60e
kaqons <123312657+kaqons@users.noreply.github.com>
2024-04-24 22:16:41 -0700

    Update layout.tsx

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -1,8 +1,1 @@
-'use client'
-
-import { AuthenticationGuard } from '@web/modules/authentication'
-import { ReactNode } from 'react'
-
-export default function LoginLayout({ children }: { children: ReactNode }) {
-  return <AuthenticationGuard isPublic={true}>{children}</AuthenticationGuard>
-}
+g
