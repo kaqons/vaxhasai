6ac4692bcb522e855577bae59b1b4525a8cca7ea
kaqons <123312657+kaqons@users.noreply.github.com>
2024-04-24 22:23:03 -0700

    Delete apps/web/src/app/(authenticated) directory

--- apps/web/src/app/(authenticated)/reset-password/[token]/components/messageSuccess/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Alert } from 'antd'
-import React from 'react'
-
-export const MessageSuccess: React.FC = () => {
-  return <Alert type="success" message="Your password has been changed." />
-}
